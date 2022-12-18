4911a51f7599d30b97eea92b6ba5d3ab2cfc5712
Randa Bukowski <randabukowski@Randas-MacBook-Pro.local>
2022-12-18 00:02:32 -0800

    tag columns

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -10,8 +10,23 @@ ProductTag.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      
-    }
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
