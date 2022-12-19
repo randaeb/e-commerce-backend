03ac3d7a2277adabd3f808a32dcef3c67ce38dd3
Randa Bukowski <randabukowski@Randas-MacBook-Pro.local>
2022-12-18 18:59:09 -0800

    tag

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -47,14 +47,43 @@ router.post('/', async (req, res) => {
 });
 
 // update a category by its `id` value
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category with this '});
+      return;
+    }
+    res.status(200).json(categoryData);
+   } catch (err) {
+    res.status(500).json(err);
+    }
+  });
+
+
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try { 
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   
-});
-
-
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,9 +4,10 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
+// find all products
   // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  
 });
 
 // get one product
